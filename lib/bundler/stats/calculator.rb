668ba39b44f26003db865caf42a20ddb379b0e31
Joseph Mastey <jmmastey@gmail.com>
2019-04-05 08:30:05 -0500

    change order of output to list worst offenders worst
    closes #6
    
    I'd previously ordered in the reverse because the worst offenders would
    be at the end of your console output. however, if you're piping to less
    or anything of the sort, this is the inverse order. I think others are
    correct about the right order.

--- lib/bundler/stats/calculator.rb
+++ lib/bundler/stats/calculator.rb
@@ -64,7 +64,7 @@ module Bundler
         stats = @gemfile.map do |gem|
           @tree.summarize(gem.name)
         end
-        stats.sort_by { |row| row[:total_dependencies] }
+        stats.sort_by { |row| row[:total_dependencies] }.reverse
       end
     end
   end
--- lib/bundler/stats/version.rb
+++ lib/bundler/stats/version.rb
@@ -1,6 +1,6 @@
 module Bundler
   module Stats
     # bundler-stats version
-    VERSION = '1.2.1'
+    VERSION = '1.3.0'
   end
 end
