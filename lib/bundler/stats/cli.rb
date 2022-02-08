bb22a79fb814ed2bb43315144e9bbf985fb140cd
Sven Schwyn <schwyn@bitcetera.com>
2022-02-08 19:21:55 +0100

    Search for alternative gems.rb and gems.locked as well

--- lib/bundler/stats/cli.rb
+++ lib/bundler/stats/cli.rb
@@ -129,19 +129,23 @@ module Bundler
       def gemfile_path
         cwd = Pathname.new("./")
         until cwd.realdirpath.root? do
-          return (cwd + "Gemfile") if File.exist?(cwd + "Gemfile")
+          %w(gems.rb Gemfile).each do |gemfile|
+            return (cwd + gemfile) if File.exist?(cwd + gemfile)
+          end
           cwd = cwd.parent
         end
-        raise ArgumentError, "Couldn't find Gemfile in this directory or parents"
+        raise ArgumentError, "Couldn't find gems.rb nor Gemfile in this directory or parents"
       end
 
       def lockfile_path
         cwd = Pathname.new(".")
         until cwd.realdirpath.root? do
-          return (cwd + "Gemfile.lock") if File.exist?(cwd + "Gemfile.lock")
+          %w(gems.locked Gemfile.lock).each do |lockfile|
+            return (cwd + lockfile) if File.exist?(cwd + lockfile)
+          end
           cwd = cwd.parent
         end
-        raise ArgumentError, "Couldn't find Gemfile in this directory or parents"
+        raise ArgumentError, "Couldn't find gems.locked nor Gemfile.lock in this directory or parents"
       end
     end
   end
